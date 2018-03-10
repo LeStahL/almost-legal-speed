2ca2589be27f8743cc0da011843c334201de61a7
Daniel Hauck <mail@dhauck.eu>
2018-03-10 12:22:32 +0100

    Add name to copyright notice.

--- Player.h
+++ Player.h
@@ -1,17 +1,18 @@
 // Almost Legal Speed - platform running game
 // Copyright (C) 2018  Alexander Kraus <nr4@z10.info>
-//                     
-// 
+//                     Daniel Hauck <mail@dhauck.eu>
+//
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -26,10 +27,10 @@ class Player
 public:
     Player();
     virtual ~Player();
-    
+
     vec2 pos, v;
     direction a;
-    
+
     double forwardPower, upwardPower, speedPower, money, brainfreeze, pizzaslow;
     bool stuck;
 };
