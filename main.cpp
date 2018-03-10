2925404cb15e09dbe80dbca1d48316b60a9254ea
Jonas Blahut <j@die-blahuts.de>
2018-03-10 12:43:43 +0100

    Added name to copyright

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // Almost Legal Speed - platform running game
 // Copyright (C) 2018  Alexander Kraus <nr4@z10.info>
 //                     Daniel Hauck <mail@dhauck.eu>
+//                     Jonas Blahut <darkphoenix@jbtec.eu>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
