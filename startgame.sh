e8914887ac60bed60f36034812dbe8edcda9178d
stadelmaier <mx@z10.info>
2018-08-08 23:26:19 +0200

    most awesome start- and installation-script

---
+++ startgame.sh
@@ -0,0 +1,88 @@
+# mx 2018
+# run script that executes the following line a little nicer
+# works only if built in "../build"
+#
+# ./speed -f /usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf -l ../levels/level04
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+
+
+# INSTALLATION
+if [ ! -d "build" ]; then
+	echo
+	echo -e "${RED}  [WARNING]${NC}"
+	echo
+	echo ">> Game not installed (correctly)"
+	echo ">> Do you want to (re-)install? (Y/N)"
+	echo
+	read -s var
+	while [[ "$var" != "y" && "$var" != "Y" && "$var" != "N" && "$var" != "n" ]]; do
+		echo ">> Type 'Y' or 'N'!"
+		read -s var
+	done
+
+	if [[ "$var" == "y" || "$var" == "Y" ]]; then
+
+		echo -e "${GREEN}  [INSTALLATION STARTED]${NC}"
+		echo
+		
+		mkdir build
+		cd build
+		cmake ..
+		if make; then
+			echo
+			echo -e "${GREEN}  [INSTALLATION SUCCEEDED]${NC}"
+			echo
+			echo ">> Installation directory is '/build/' "
+			echo
+		fi
+
+	else 
+		echo 
+		echo ">> Bye Bye"
+		echo
+		exit 1
+	fi
+		
+fi
+
+if [ -d "build" ]; then
+	echo
+	echo -e "${GREEN}  [WELCOME!]${NC}"
+	echo 
+fi
+
+# RUNNING THE MOTHERFUCKING GAME
+
+LEVEL="$1"
+FONT="$2"
+
+if [ -z "$LEVEL"]; then
+	LEVEL="4"
+fi
+
+if [ -z "$FONT" ]; then
+	FONT="../gfx/fonts/FreeSans.ttf"
+fi 
+
+echo Level $LEVEL
+echo with Font $FONT
+
+cd build
+if ./speed -f $FONT -l ../levels/level0$LEVEL; then
+	echo
+	echo -e "${GREEN}  [GOODBYE]${NC}"
+	echo ">> Thanks for playing"
+	echo
+else
+	echo -e "${RED}  [WARNING]${NC}"
+	echo ">> Broken installation"
+	echo ">> Please try again after removing 'build' directory"
+	echo
+	echo ">> Please check that the following 'curl' and 'smfl' are installed:"
+	echo ">> try 'sudo apt-get install libcurl4-gnutls-dev libsfml-dev'"
+	echo
+
+fi
+
